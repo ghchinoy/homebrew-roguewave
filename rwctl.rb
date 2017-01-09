5c88e46188a2cfa13c481ac2a7b273be33a03910
G. Hussain Chinoy <ghchinoy@gmail.com>
2017-01-08 17:56:24 -0700

    updating rwctl.rb

--- rwctl.rb
+++ rwctl.rb
@@ -1,7 +1,7 @@
 class Rwctl < Formula
   desc "Rogue Wave API Platform CLI"
   homepage "https://github.com/ghchinoy/rwctl"
-  url "https://github.com/ghchinoy/rwctl/releases/download/v0.1.0/atmotool-0.1.0.tar.gz"
+  url "https://github.com/ghchinoy/rwctl/releases/download/v0.1.0/rwctl-0.1.0.tar.gz"
   sha256 "6be647204f357b9511ca4fba09956021d8feb1093de087a6e84edd319c63605f"
 
   bottle :unneeded
