acddb8c9ba445b6159e1c25271b427e996f8d9e8
G. Hussain Chinoy <ghchinoy@gmail.com>
2017-01-09 21:52:27 -0700

    0.3.0

--- rwctl.rb
+++ rwctl.rb
@@ -1,8 +1,8 @@
 class Rwctl < Formula
   desc "Rogue Wave API Platform CLI"
   homepage "https://github.com/ghchinoy/rwctl"
-  url "https://github.com/ghchinoy/rwctl/releases/download/v0.2.0/rwctl-0.2.0.tar.gz"
-  sha256 "7d0bc8ed2e92f53d5b3e930fda39909cbd0acd71572015de08b0a350a0a1f72b"
+  url "https://github.com/ghchinoy/rwctl/releases/download/v0.3.0/rwctl-0.3.0.tar.gz"
+  sha256 "f416698b67f57e6279306302b0050ee47cbe73e6b1841d44de9373cd874a59da"
 
   bottle :unneeded
 
