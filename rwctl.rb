4bca387f2ceed7071a663ddae02f181c7ac63d74
G. Hussain Chinoy <ghchinoy@gmail.com>
2017-01-09 22:02:05 -0700

    reverting to v0.2.1 as per roadmap

--- rwctl.rb
+++ rwctl.rb
@@ -1,8 +1,8 @@
 class Rwctl < Formula
   desc "Rogue Wave API Platform CLI"
   homepage "https://github.com/ghchinoy/rwctl"
-  url "https://github.com/ghchinoy/rwctl/releases/download/v0.3.0/rwctl-0.3.0.tar.gz"
-  sha256 "f416698b67f57e6279306302b0050ee47cbe73e6b1841d44de9373cd874a59da"
+  url "https://github.com/ghchinoy/rwctl/releases/download/v0.2.1/rwctl-0.2.1.tar.gz"
+  sha256 "82f1dbd152b40f3fb55d058e842268b3b9f4a8b5df77c9f93205ab0db2b7f5c2"
 
   bottle :unneeded
 
